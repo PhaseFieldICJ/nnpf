e0742aa181d50832ff5f61a2f76eb6b31c3fc3e0
Roland DENIS <denis@math.univ-lyon1.fr>
2020-06-29 22:50:48 +0200

    Distance to a shape in a periodic domain

--- shapes.py
+++ shapes.py
@@ -3,6 +3,8 @@ Signed distances functions and tools
 
 Example
 -------
+
+Centered sphere:
 >>> X, Y = torch.meshgrid(torch.linspace(0, 1, 5), torch.linspace(0, 1, 5))
 >>> s = sphere([0.5, 0.5], 0.25)
 >>> s(X, Y)
@@ -11,6 +13,24 @@ tensor([[ 0.4571,  0.3090,  0.2500,  0.3090,  0.4571],
         [ 0.2500,  0.0000, -0.2500,  0.0000,  0.2500],
         [ 0.3090,  0.1036,  0.0000,  0.1036,  0.3090],
         [ 0.4571,  0.3090,  0.2500,  0.3090,  0.4571]])
+
+Shifted sphere with periodicity:
+>>> sp = periodic(sphere([0.75, 0.25], 0.25), [(0, 1), (0, 1)])
+>>> sp(X, Y)
+tensor([[ 0.1036,  0.0000,  0.1036,  0.3090,  0.1036],
+        [ 0.3090,  0.2500,  0.3090,  0.4571,  0.3090],
+        [ 0.1036,  0.0000,  0.1036,  0.3090,  0.1036],
+        [ 0.0000, -0.2500,  0.0000,  0.2500,  0.0000],
+        [ 0.1036,  0.0000,  0.1036,  0.3090,  0.1036]])
+
+Distance calculation for multiple spheres at once:
+>>> center = torch.rand(2, 10, 1, 1)
+>>> radius = torch.rand(10, 1, 1)
+>>> sm = sphere(center, radius)
+>>> sm(X, Y).shape
+torch.Size([10, 5, 5])
+
+Distance to two spheres:
 >>> X, Y = torch.meshgrid(torch.linspace(0, 2, 9), torch.linspace(0, 1, 5))
 >>> s2 = union(s, translation(s, (1, 0)))
 >>> s2(X, Y)
@@ -41,20 +61,24 @@ def sphere(center, radius):
 ###############################################################################
 # Operations
 
-def union(*shapes):
-    """ Union of shapes (not exact) """
+def reduce(op, *shapes):
+    """ Reduce an operator over multiple shapes """
     def dist(*X):
-        return torch.min(*(shape(*X) for shape in shapes))
+        result = shapes[0](*X)
+        for s in shapes[1:]:
+            result = op(result, s(*X))
+        return result
 
     return dist
+
+def union(*shapes):
+    """ Union of shapes (not exact) """
+    return reduce(torch.min, *shapes)
 
 
 def intersection(*shapes):
     """ Intersection of shapes (not exact) """
-    def dist(*X):
-        return torch.max(*(shape(*X) for shape in shapes))
-
-    return dist
+    return reduce(torch.max, *shapes)
 
 
 def translation(shape, shift):
@@ -65,4 +89,21 @@ def translation(shape, shift):
     return dist
 
 
+def periodic(shape, bounds):
+    """ Periodicize a shape using given bounds
+
+    None bound means no periodicity.
+    """
+
+    def shift_gen(shift=[]):
+        i = len(shift)
+        if i == len(bounds):
+            yield shift
+        elif bounds[i] is None:
+            yield from shift_gen(shift + [0.])
+        else:
+            for sign in (-1., 0., 1.):
+                yield from shift_gen(shift + [sign * (bounds[i][1] - bounds[i][0])])
+
+    return union(*(translation(shape, shift) for shift in shift_gen()))
 
