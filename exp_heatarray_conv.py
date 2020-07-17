118c5c4d52766a8579e751966d1cb97569c75334
Roland DENIS <denis@math.univ-lyon1.fr>
2020-07-17 15:56:33 +0200

    Add experiment script about convergence depending on loss def

---
+++ exp_heatarray_conv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import itertools
+import argparse
+
+from heat_array_model import HeatArray
+from trainer import Trainer
+
+# Experiment configuration
+init_list = ['zeros']
+loss_norms_list = [[(1, 1)], [(2, 1)], [(float('inf'), 1)]]
+loss_power_list = [1, 2, 3, 4]
+lr_exp_list = [2, 3, 4, 5]
+
+# Command-line arguments
+parser = argparse.ArgumentParser(
+    description="Experiment on the loss formulation",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = Trainer.add_argparse_args(parser)
+parser = HeatArray.add_model_specific_args(parser)
+args = parser.parse_args()
+kwargs = vars(args)
+
+# Main loop
+for (init, loss_norms, loss_power, lr_exp) in itertools.product(init_list, loss_norms_list, loss_power_list, lr_exp_list):
+
+    kwargs.update({
+        'init': init,
+        'loss_norms': loss_norms,
+        'loss_power': loss_power,
+        'lr': 10**(-lr_exp),
+        'version': f"{init}_l{loss_norms[0][0]}_p{loss_power}_lr{lr_exp}",
+    })
+
+    print(f"version: {kwargs['version']}")
+
+    model = HeatArray(**kwargs)
+    trainer = Trainer.from_argparse_args(args, "HeatArrayExperiment")
+    trainer.fit(model)
+
