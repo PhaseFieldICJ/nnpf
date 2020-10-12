c772beea72af4c96d19d6e7446d858f18dd9d27f
Roland DENIS <denis@math.univ-lyon1.fr>
2020-10-12 17:15:30 +0200

    Progress bar, offscreen rendering and gpu eval in AC animation

--- anim_allen_cahn.py
+++ anim_allen_cahn.py
@@ -7,11 +7,10 @@ from allen_cahn_problem import AllenCahnProblem
 import shapes
 import visu
 import phase_field as pf
-import matplotlib.pyplot as plt
 import argparse
 import imageio
-import numpy as np
 import torch
+import tqdm
 
 # Command-line arguments
 parser = argparse.ArgumentParser()
@@ -19,12 +18,23 @@ parser.add_argument("checkpoint", type=str, help="Path to the model's checkpoint
 parser.add_argument("--no_save", action="store_true", help="Don't save the animation")
 parser.add_argument("--tol", type=float, default=1e-5, help="Tolerance used as a stop criteron")
 parser.add_argument("--no_dist", action="store_true", help="Display phase field instead of distance")
+parser.add_argument("--offscreen", action="store_true", help="Don't display the animation (but still saving")
+parser.add_argument("--gpu", action="store_true", help="Evaluation model on your GPU")
 
 args = parser.parse_args()
+
+# Matplotlib rendering backend
+if args.offscreen:
+    import matplotlib as mpl
+    mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 # Loading model
 model = AllenCahnProblem.load_from_checkpoint(args.checkpoint, map_location=torch.device("cpu"))
 model.freeze()
+
+if args.gpu:
+    model.cuda()
 
 # Defining initial shape
 bounds = model.domain.bounds
@@ -58,11 +68,11 @@ plt.figure(figsize=(6, 6))
 
 if args.no_dist:
     def data_from(u):
-        return u
+        return u.cpu()
     graph = visu.PhaseFieldShow(data_from(u), extent=extent, interpolation=interpolation)
 else:
     def data_from(u):
-        return pf.iprofil(u, model.hparams.epsilon)
+        return pf.iprofil(u, model.hparams.epsilon).cpu()
     graph = visu.DistanceShow(data_from(u), scale=scale, extent=extent, interpolation=interpolation)
 
 
@@ -75,20 +85,26 @@ with visu.AnimWriter('anim.avi', fps=25, do_nothing=args.no_save) as anim:
     for i in range(25):
         anim.add_frame()
 
-    last_error = [0] * 25
+    last_diff = [args.tol + 1] * 25
 
-    for i in range(10000):
-        last_u = u.clone()
-        u = model(u[None, None, ...])[0, 0, ...]
+    with tqdm.tqdm() as pbar:
+        while max(last_diff) > args.tol:
+            last_u = u.clone()
+            u = model(u[None, None, ...])[0, 0, ...]
 
-        graph.update(data_from(u))
-        title.set_text(f"t = {i*model.hparams.dt:.5} ; it = {i}")
-        plt.pause(0.01)
+            graph.update(data_from(u))
+            title.set_text(f"t = {i*model.hparams.dt:.5} ; it = {i}")
+            plt.pause(0.01)
 
-        anim.add_frame()
+            anim.add_frame()
 
-        last_error[1:] = last_error[:-1]
-        last_error[0] = (u - last_u).norm()
-        if max(last_error) <= args.tol:
-            break
+            vol = model.domain.dX.prod() * u.sum()
+            last_diff[1:] = last_diff[:-1]
+            last_diff[0] = (u - last_u).norm()
 
+            pbar.update(1)
+            pbar.set_postfix({
+                'volume': vol.item(),
+                'diff': last_diff[0].item(),
+                'max diff': max(last_diff).item()
+            })
