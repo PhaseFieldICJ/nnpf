6a06c2f5edb73b35931782ab7e6065f9bbd5ec54
Roland DENIS <denis@math.univ-lyon1.fr>
2021-05-03 16:22:40 +0200

    Subplots in mean curvature animation and custom lp for shapes and check

--- scripts/anim_mean_curvature_problem.py
+++ scripts/anim_mean_curvature_problem.py
@@ -25,9 +25,11 @@ parser.add_argument("--tol", type=float, default=1e-5, help="Tolerance used as a
 parser.add_argument("--max_it", type=int, default=-1, help="Maximum number of calculated iterations (-1 for illimited")
 parser.add_argument("--max_frames", type=int, default=-1, help="Maximum number of rendered frames (-1 for illimited)")
 parser.add_argument("--max_duration", type=float, default=-1, help="Maximum duration of the animation (-1 for illimited")
-parser.add_argument("--no_dist", action="store_true", help="Display phase field instead of distance")
+parser.add_argument("--scalars", type=str, choices=["u", "dist", "check"], nargs='*', default=["dist"], help="Scalars to be displayed")
 parser.add_argument("--scale", type=float, default=1., help="Initial shape scale")
 parser.add_argument("--shape", type=str, choices=["one", "two", "three"], default="one", help="Initial shape")
+parser.add_argument("--lp_shape", type=float, help="lp norm used to define the initial shapes (default to model.hparams.lp)")
+parser.add_argument("--lp_check", type=float, help="lp norm used to check the distance (default to model.hparams.lp)")
 parser.add_argument("--offscreen", action="store_true", help="Don't display the animation (but still saving")
 parser.add_argument("--gpu", action="store_true", help="Evaluation model on your GPU")
 parser.add_argument("--display_step", type=int, default=1, help="Render frame every given number")
@@ -60,6 +62,8 @@ domain = model.domain
 bounds = domain.bounds
 domain_extent = [b[1] - b[0] for b in bounds]
 domain_diameter = min(domain_extent)
+args.lp_shape = args.lp_shape or model.hparams.lp
+args.lp_check = args.lp_check or model.hparams.lp
 
 def radius(r, scale):
     return scale * r * domain_diameter
@@ -78,8 +82,8 @@ elif args.shape == "two":
 elif args.shape == "three":
     spheres = [(0.1, 0.2, 0.2), (0.2, 0.3, 0.7), (0.05, 0.7, 0.3)]
 
-s = shapes.union(*(shapes.sphere(radius(p[0], args.scale), pos(p[1:], args.scale), model.hparams.lp) for p in spheres))
-dist_sol = lambda t: reduce(torch.min, [shapes.sphere(model.sphere_radius(radius(p[0], args.scale), t), pos(p[1:], args.scale), model.hparams.lp)(*domain.X) for p in spheres])
+s = shapes.union(*(shapes.sphere(radius(p[0], args.scale), pos(p[1:], args.scale), args.lp_shape) for p in spheres))
+dist_sol = lambda t: reduce(torch.min, [shapes.sphere(model.sphere_radius(radius(p[0], args.scale), t), pos(p[1:], args.scale), args.lp_shape)(*domain.X) for p in spheres])
 
 # Periodizing
 s = shapes.periodic(s, bounds)
@@ -94,20 +98,38 @@ scale = 0.25 * max(b[1] - b[0] for b, n in zip(domain.bounds, domain.N))
 extent = [*domain.bounds[0], *domain.bounds[1]]
 interpolation = "kaiser"
 
-plt.figure(figsize=args.figsize)
+fig = plt.figure(figsize=(args.figsize[0] * len(args.scalars), args.figsize[0]))
+subplots = []
 
-if args.no_dist:
+if "u" in args.scalars:
+    plt.subplot(1, len(args.scalars), len(subplots) + 1)
     def data_from(u):
         return u.cpu()
     graph = visu.PhaseFieldShow(data_from(u), extent=extent, interpolation=interpolation)
-else:
+    contour = visu.ContourShow(dist_sol(0.).cpu(), [0.], X=[x.cpu() for x in domain.X], colors='red')
+    subplots.append((data_from, graph, contour))
+    plt.title("u")
+
+if "dist" in args.scalars:
+    plt.subplot(1, len(args.scalars), len(subplots) + 1)
     def data_from(u):
         return model.iprofil(u, model.hparams.epsilon).cpu()
     graph = visu.DistanceShow(data_from(u), scale=scale, extent=extent, interpolation=interpolation)
+    contour = visu.ContourShow(dist_sol(0.).cpu(), [0.], X=[x.cpu() for x in domain.X], colors='red')
+    subplots.append((data_from, graph, contour))
+    plt.title("distance")
 
-contour = visu.ContourShow(dist_sol(0.).cpu(), [0.], X=[x.cpu() for x in domain.X], colors='red')
+if "check" in args.scalars:
+    plt.subplot(1, len(args.scalars), len(subplots) + 1)
+    def data_from(u):
+        return shapes.check_dist(model.iprofil(u, model.hparams.epsilon).cpu(), domain.dX, p=args.lp_check)
+    graph = visu.ImShow(data_from(u), extent=extent, interpolation=interpolation, vmin=0, vmax=2, cmap='seismic')
+    contour = visu.ContourShow(dist_sol(0.).cpu(), [0.], X=[x.cpu() for x in domain.X], colors='red')
+    subplots.append((data_from, graph, contour))
+    #plt.colorbar(graph.mappable)
+    plt.title("distance check")
 
-title = plt.title(f"t = 0 ; it = 0")
+title = fig.suptitle(f"t = 0 ; it = 0")
 plt.tight_layout()
 plt.pause(1)
 
@@ -124,8 +146,9 @@ with visu.AnimWriter(args.output, fps=args.fps, do_nothing=args.no_save) as anim
             u = model(u[None, None, ...])[0, 0, ...]
 
             if pbar.n % args.display_step == 0:
-                graph.update(data_from(u))
-                contour.update(dist_sol(pbar.n * model.hparams.dt).cpu())
+                for data_from, graph, contour in subplots:
+                    graph.update(data_from(u))
+                    contour.update(dist_sol(pbar.n * model.hparams.dt).cpu())
                 title.set_text(f"t = {pbar.n*model.hparams.dt:.5} ; it = {pbar.n}")
                 plt.pause(0.01)
 
