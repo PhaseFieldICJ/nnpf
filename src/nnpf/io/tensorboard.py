7589337ed731f3e40f11716145a94757c898b1d8
Roland DENIS <denis@math.univ-lyon1.fr>
2021-05-12 09:20:12 +0200

    Adding doctest for TensorBoardScalars
    BTW checking the first validation loss

--- src/nnpf/io/tensorboard.py
+++ src/nnpf/io/tensorboard.py
@@ -11,6 +11,30 @@ class TensorBoardScalars:
     ----------
     path: str
         Event file path or folder path containing one or more event files.
+
+    Examples
+    --------
+
+    Training:
+    >>> from nnpf.trainer import Trainer
+    >>> from nnpf.models import Reaction
+    >>> trainer = Trainer(default_root_dir="logs_doctest", name="Reaction", version="test_tbs", max_epochs=10)
+    >>> model = Reaction(train_N=10, val_N=20, seed=0)
+    >>> import contextlib, io
+    >>> with contextlib.redirect_stdout(io.StringIO()):
+    ...     with contextlib.redirect_stderr(io.StringIO()):
+    ...         trainer.fit(model)
+
+    Reading events:
+    >>> import os
+    >>> tbs = TensorBoardScalars(os.path.join("logs_doctest", "Reaction", "test_tbs"))
+    >>> sorted(tbs.scalars)
+    ['epoch', 'hp_metric', 'train_loss', 'val_loss']
+    >>> wall_time, rel_time, step, value = tbs["val_loss"]
+    >>> value
+    array([1.67743802, 1.65534616, 1.63326979, 1.61121798, 1.5891999 ,
+           1.5672245 , 1.54530096, 1.52343798, 1.50164437, 1.47992933,
+           1.45830166])
     """
 
     def __init__(self, path):
